3e9dcb10552345bec4db6aa85eee97208e6e917e
Tony <santiago.montesb@gmail.com>
2024-02-18 21:18:43 -0500

    gps-comparision

---
+++ individual-tests/gps/gps-comparision/gps-comparision.ino
@@ -0,0 +1,44 @@
+#include "TinyGPSPlus.h"
+#include "SoftwareSerial.h"
+
+SoftwareSerial serial_connection(16,17);
+TinyGPSPlus gps;
+
+unsigned long lastConnectionTime = 0; // Variable to track the last connection time
+const unsigned long connectionInterval = 1000; // Interval between connection checks in milliseconds
+int i = 0;
+
+void setup() {
+  Serial.begin(9600);
+  serial_connection.begin(9600);
+  Serial.println("GPS Start");
+}
+
+void loop() {
+  while (serial_connection.available()) {
+    gps.encode(serial_connection.read());
+  }
+
+  if (gps.location.isUpdated()) {
+    i=0;
+    Serial.print("Satellite: ");
+    Serial.println(gps.satellites.value());
+    Serial.print("Lat: ");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(", Lon: ");
+    Serial.println(gps.location.lng(), 6);
+    Serial.print("Speed MPH: ");
+    Serial.println(gps.speed.mph());
+    Serial.print("Altitude meter: ");
+    Serial.println(gps.altitude.meters());
+    Serial.println("-------------------------");
+  } else {
+    if (millis() - lastConnectionTime >= connectionInterval) {
+      i++;
+      Serial.println("connecting... ("+String(i)+")");
+      lastConnectionTime = millis(); // Update the last connection time
+    }
+  }
+}
+
+
