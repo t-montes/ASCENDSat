3995c7a70f2036b808ec98ba9c1be39485046d63
Tony <santiago.montesb@gmail.com>
2024-02-29 22:14:57 -0500

    sensors code v0.1

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,10 +1,6 @@
-/* ----------------------- LIBRARIES ----------------------- */
+/* ------------------------ LIBRARIES ------------------------- */
 
-// General
-#include <Wire.h>
-#include "SPI.h"
-
-// MicroSD and Camera
+// MicroSD and Camera [OV2640]
 #include "esp_camera.h"
 #include "Arduino.h"
 #include "FS.h"
@@ -12,160 +8,134 @@
 #include "soc/soc.h"
 #include "soc/rtc_cntl_reg.h"
 #include "driver/rtc_io.h"
+#include <EEPROM.h> 
 
-// Barometer
-#include <Adafruit_Sensor.h>
-#include "Adafruit_BMP280.h"
+/* ------------------- CONSTANTS/VARIABLES -------------------- */
 
-// GPS
-#include "TinyGPSPlus.h"
-#include "SoftwareSerial.h"
+// MicroSD and Camera
+#define EEPROM_SIZE 1
 
-/* ------------------ CONSTANTS/VARIABLES ------------------ */
+#define PWDN_GPIO_NUM     32
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM      0
+#define SIOD_GPIO_NUM     26
+#define SIOC_GPIO_NUM     27
 
-// MicroSD and Camera in ESP32
-#define PWDN_GPIO_NUM 32
-#define RESET_GPIO_NUM -1
-#define XCLK_GPIO_NUM 0
-#define SIOD_GPIO_NUM 26
-#define SIOC_GPIO_NUM 27
+#define Y9_GPIO_NUM       35
+#define Y8_GPIO_NUM       34
+#define Y7_GPIO_NUM       39
+#define Y6_GPIO_NUM       36
+#define Y5_GPIO_NUM       21
+#define Y4_GPIO_NUM       19
+#define Y3_GPIO_NUM       18
+#define Y2_GPIO_NUM        5
+#define VSYNC_GPIO_NUM    25
+#define HREF_GPIO_NUM     23
+#define PCLK_GPIO_NUM     22
 
-#define Y9_GPIO_NUM 35
-#define Y8_GPIO_NUM 34
-#define Y7_GPIO_NUM 39
-#define Y6_GPIO_NUM 36
-#define Y5_GPIO_NUM 21
-#define Y4_GPIO_NUM 19
-#define Y3_GPIO_NUM 18
-#define Y2_GPIO_NUM 5
-#define VSYNC_GPIO_NUM 25
-#define HREF_GPIO_NUM 23
-#define PCLK_GPIO_NUM 22
+int pictureNumber = 0;
 
-// Barometer [BMP280]
-Adafruit_BMP280 bmp;
-float pressure;
-float temperature;
-int altitude;
+/* -------------------- AUXILIAR FUNCTIONS -------------------- */
 
-// GPS [GT-U7]
-#define TX_PIN 1
-#define RX_PIN 3
-SoftwareSerial serial_connection(RX_PIN, TX_PIN);
-TinyGPSPlus gps;
-float latitude;
-float longitude;
-float altitude;
 
-/* -------------------- MEASURE FUNCTIONS -------------------- */
-
-void take_and_save_picture() {
-    camera_fb_t *fb = NULL;
-    fb = esp_camera_fb_get();
-    if (!fb) {
-        Serial.println("Camera capture failed");
-        return;
-    }
-
-    String path = "/picture.jpg";
-    fs::FS &fs = SD_MMC;
-    File file = fs.open(path.c_str(), FILE_WRITE);
-    if (!file) {
-        Serial.println("Failed to open file in writing mode");
-    }
-    else {
-        file.write(fb->buf, fb->len);
-        Serial.printf("Saved picture to path: %s\n", path.c_str());
-    }
-    file.close();
-    esp_camera_fb_return(fb);
-
-    digitalWrite(4, LOW);
-    rtc_gpio_hold_en(GPIO_NUM_4);
-}
-
-void measure_barometer() {
-    pressure = bmp.readPressure();
-    temperature = bmp.readTemperature();
-    altitude = bmp.readAltitude(1013.25);
-}
-
-void measure_gps() {
-    if (gps.location.isUpdated()) {
-        latitude = gps.location.lat();
-        longitude = gps.location.lng();
-        altitude = gps.altitude.meters();
-    }
-}
-
-/* -------------------------- SETUP -------------------------- */
+/* -------------------------- SETUP --------------------------- */
 
 void setup() {
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);
-    Serial.begin(115200);
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+  Serial.begin(115200);
+  
+  // MicroSD and Camera
+  camera_config_t config;
+  config.ledc_channel = LEDC_CHANNEL_0;
+  config.ledc_timer = LEDC_TIMER_0;
+  config.pin_d0 = Y2_GPIO_NUM;
+  config.pin_d1 = Y3_GPIO_NUM;
+  config.pin_d2 = Y4_GPIO_NUM;
+  config.pin_d3 = Y5_GPIO_NUM;
+  config.pin_d4 = Y6_GPIO_NUM;
+  config.pin_d5 = Y7_GPIO_NUM;
+  config.pin_d6 = Y8_GPIO_NUM;
+  config.pin_d7 = Y9_GPIO_NUM;
+  config.pin_xclk = XCLK_GPIO_NUM;
+  config.pin_pclk = PCLK_GPIO_NUM;
+  config.pin_vsync = VSYNC_GPIO_NUM;
+  config.pin_href = HREF_GPIO_NUM;
+  config.pin_sscb_sda = SIOD_GPIO_NUM;
+  config.pin_sscb_scl = SIOC_GPIO_NUM;
+  config.pin_pwdn = PWDN_GPIO_NUM;
+  config.pin_reset = RESET_GPIO_NUM;
+  config.xclk_freq_hz = 20000000;
+  config.pixel_format = PIXFORMAT_JPEG; 
+  
+  if(psramFound()){
+    config.frame_size = FRAMESIZE_UXGA;
+    config.jpeg_quality = 10;
+    config.fb_count = 2;
+  } else {
+    config.frame_size = FRAMESIZE_SVGA;
+    config.jpeg_quality = 12;
+    config.fb_count = 1;
+  }
+  
+  esp_err_t err = esp_camera_init(&config);
+  if (err != ESP_OK) {
+    Serial.printf("Camera init failed with error 0x%x", err);
+    return;
+  }
+  
+  if(!SD_MMC.begin()){
+    Serial.println("SD Card Mount Failed");
+    return;
+  }
+  
+  uint8_t cardType = SD_MMC.cardType();
+  if(cardType == CARD_NONE){
+    Serial.println("No SD Card attached");
+    return;
+  }
+    
+  camera_fb_t * fb = NULL;
+  fb = esp_camera_fb_get();  
+  if(!fb) {
+    Serial.println("Camera capture failed");
+    return;
+  }
 
-    // MicroSD and Camera
-    camera_config_t config;
-    config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer = LEDC_TIMER_0;
-    config.pin_d0 = Y2_GPIO_NUM;
-    config.pin_d1 = Y3_GPIO_NUM;
-    config.pin_d2 = Y4_GPIO_NUM;
-    config.pin_d3 = Y5_GPIO_NUM;
-    config.pin_d4 = Y6_GPIO_NUM;
-    config.pin_d5 = Y7_GPIO_NUM;
-    config.pin_d6 = Y8_GPIO_NUM;
-    config.pin_d7 = Y9_GPIO_NUM;
-    config.pin_xclk = XCLK_GPIO_NUM;
-    config.pin_pclk = PCLK_GPIO_NUM;
-    config.pin_vsync = VSYNC_GPIO_NUM;
-    config.pin_href = HREF_GPIO_NUM;
-    config.pin_sscb_sda = SIOD_GPIO_NUM;
-    config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn = PWDN_GPIO_NUM;
-    config.pin_reset = RESET_GPIO_NUM;
-    config.xclk_freq_hz = 20000000;
-    config.pixel_format = PIXFORMAT_JPEG;
+  EEPROM.begin(EEPROM_SIZE);
+  pictureNumber = EEPROM.read(0) + 1;
 
-    if (psramFound()) {
-        config.frame_size = FRAMESIZE_UXGA;
-        config.jpeg_quality = 10;
-        config.fb_count = 2;
-    }
-    else {
-        config.frame_size = FRAMESIZE_SVGA;
-        config.jpeg_quality = 12;
-        config.fb_count = 1;
-    }
+  String path = "/picture" + String(pictureNumber) +".jpg";
+  fs::FS &fs = SD_MMC; 
+  Serial.printf("Picture file name: %s\n", path.c_str());
+  
+  File file = fs.open(path.c_str(), FILE_WRITE);
+  if(!file){
+    Serial.println("Failed to open file in writing mode");
+  } 
+  else {
+    file.write(fb->buf, fb->len);
+    Serial.printf("Saved file to path: %s\n", path.c_str());
+    EEPROM.write(0, pictureNumber);
+    EEPROM.commit();
+  }
+  file.close();
+  esp_camera_fb_return(fb); 
+  
+  // Turn off the FLASH led
+  pinMode(4, OUTPUT);
+  digitalWrite(4, LOW);
+  rtc_gpio_hold_en(GPIO_NUM_4);
 
-    esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
-        Serial.printf("Camera init failed with error 0x%x", err);
-        return;
-    }
-
-    if (!SD_MMC.begin()) {
-        Serial.println("SD Card Mount Failed");
-        return;
-    }
-
-    uint8_t cardType = SD_MMC.cardType();
-    if (cardType == CARD_NONE) {
-        Serial.println("No SD Card attached");
-        return;
-    }
-
-    pinMode(4, OUTPUT);
-
-    // Barometer
-    bmp.begin();
-
-    // GPS
-    serial_connection.begin(9600);
+  // Go to sleep mode  
+  delay(2000);
+  Serial.println("Going to sleep now");
+  delay(2000);
+  esp_deep_sleep_start();
+  Serial.println("This will never be printed");
 }
 
+/* --------------------------- LOOP --------------------------- */
+
 void loop() {
-    while (serial_connection.available()) {
-        gps.encode(serial_connection.read());
-    }
+  
 }
