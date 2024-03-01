ddbb2e4b386281dd3366f949978590e66cac9759
Tony <santiago.montesb@gmail.com>
2024-03-01 11:48:24 -0500

    S3 sensors v0.1

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -1,62 +1,61 @@
-/* ------------------------ LIBRARIES ------------------------- */
-
-// MicroSD and Camera [OV2640]
 #include "esp_camera.h"
-#include "Arduino.h"
 #include "FS.h"
-#include "SD_MMC.h"
-#include "soc/soc.h"
-#include "soc/rtc_cntl_reg.h"
-#include "driver/rtc_io.h"
+#include "SD.h"
+#include "SPI.h"
+#include <TimeLib.h>
 
-/* ------------------- CONSTANTS/VARIABLES -------------------- */
+#define CAMERA_MODEL_XIAO_ESP32S3
 
-// MicroSD and Camera
-#define PWDN_GPIO_NUM     32
-#define RESET_GPIO_NUM    -1
-#define XCLK_GPIO_NUM      0
-#define SIOD_GPIO_NUM     26
-#define SIOC_GPIO_NUM     27
+#include "camera_pins.h"
 
-#define Y9_GPIO_NUM       35
-#define Y8_GPIO_NUM       34
-#define Y7_GPIO_NUM       39
-#define Y6_GPIO_NUM       36
-#define Y5_GPIO_NUM       21
-#define Y4_GPIO_NUM       19
-#define Y3_GPIO_NUM       18
-#define Y2_GPIO_NUM        5
-#define VSYNC_GPIO_NUM    25
-#define HREF_GPIO_NUM     23
-#define PCLK_GPIO_NUM     22
+unsigned long lastCaptureTime = 0;
+int imageCount = 1;
+bool camera_sign = false;
+bool sd_sign = false;
 
-/* -------------------- AUXILIAR FUNCTIONS -------------------- */
-
-void save_to_file(String filename, String data, char mode) {
-  File file;
-
-  if (mode == 'a') {
-    file = SD_MMC.open(filename, FILE_APPEND);
-  } else {
-    file = SD_MMC.open(filename, FILE_WRITE);
+void photo_save(const char * fileName) {
+  camera_fb_t *fb = esp_camera_fb_get();
+  if (!fb) {
+    Serial.println("Failed to get camera frame buffer");
+    return;
   }
+  writeFile(SD, fileName, fb->buf, fb->len);
+  
+  esp_camera_fb_return(fb);
+  Serial.println("Photo saved to file");
+}
 
+void writeFile(fs::FS &fs, const char * path, uint8_t * data, size_t len){
+    Serial.printf("Writing file: %s\r\n", path);
+
+    File file = fs.open(path, FILE_WRITE);
+    if(!file){
+        Serial.println("Failed to open file for writing");
+        return;
+    }
+    if(file.write(data, len) == len){
+        Serial.println("File written");
+    } else {
+        Serial.println("Write failed");
+    }
+    file.close();
+}
+
+void appendFile(const char * path, const char * data) {
+  File file = SD.open(path, FILE_APPEND);
   if (file) {
     file.println(data);
     file.close();
-    Serial.println("Data appended/written to file successfully!");
+    Serial.println("Data appended to file");
   } else {
-    Serial.println("Failed to open file for writing/appending");
+    Serial.println("Failed to open file for appending");
   }
 }
 
-/* -------------------------- SETUP --------------------------- */
-
 void setup() {
-  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
   Serial.begin(115200);
-  
-  // MicroSD and Camera
+  while(!Serial);
+
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
   config.ledc_timer = LEDC_TIMER_0;
@@ -77,77 +76,86 @@ void setup() {
   config.pin_pwdn = PWDN_GPIO_NUM;
   config.pin_reset = RESET_GPIO_NUM;
   config.xclk_freq_hz = 20000000;
-  config.pixel_format = PIXFORMAT_JPEG; 
+  config.frame_size = FRAMESIZE_UXGA;
+  config.pixel_format = PIXFORMAT_JPEG; // for streaming
+  config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
+  config.fb_location = CAMERA_FB_IN_PSRAM;
+  config.jpeg_quality = 12;
+  config.fb_count = 1;
   
-  if(psramFound()){
-    config.frame_size = FRAMESIZE_UXGA;
-    config.jpeg_quality = 10;
-    config.fb_count = 2;
+  if(config.pixel_format == PIXFORMAT_JPEG){
+    if(psramFound()){
+      config.jpeg_quality = 10;
+      config.fb_count = 2;
+      config.grab_mode = CAMERA_GRAB_LATEST;
+    } else {
+      config.frame_size = FRAMESIZE_SVGA;
+      config.fb_location = CAMERA_FB_IN_DRAM;
+    }
   } else {
-    config.frame_size = FRAMESIZE_SVGA;
-    config.jpeg_quality = 12;
-    config.fb_count = 1;
+    config.frame_size = FRAMESIZE_240X240;
+    #if CONFIG_IDF_TARGET_ESP32S3
+        config.fb_count = 2;
+    #endif
   }
-  
+
+  // camera init
   esp_err_t err = esp_camera_init(&config);
   if (err != ESP_OK) {
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
   
-  if(!SD_MMC.begin()){
-    Serial.println("SD Card Mount Failed");
+  camera_sign = true;
+
+  if(!SD.begin(21)){
+    Serial.println("Card Mount Failed");
     return;
   }
-  
-  uint8_t cardType = SD_MMC.cardType();
+  uint8_t cardType = SD.cardType();
+
   if(cardType == CARD_NONE){
-    Serial.println("No SD Card attached");
-    return;
-  }
-    
-  camera_fb_t * fb = NULL;
-  fb = esp_camera_fb_get();  
-  if(!fb) {
-    Serial.println("Camera capture failed");
+    Serial.println("No SD card attached");
     return;
   }
 
-  String path = "/MYpicture.jpg";
-  fs::FS &fs = SD_MMC; 
-  Serial.printf("Picture file name: %s\n", path.c_str());
-  
-  File file = fs.open(path.c_str(), FILE_WRITE);
-  if(!file){
-    Serial.println("Failed to open file in writing mode");
-  } 
-  else {
-    file.write(fb->buf, fb->len);
-    Serial.printf("Saved file to path: %s\n", path.c_str());
+  Serial.print("SD Card Type: ");
+  if(cardType == CARD_MMC){
+    Serial.println("MMC");
+  } else if(cardType == CARD_SD){
+    Serial.println("SDSC");
+  } else if(cardType == CARD_SDHC){
+    Serial.println("SDHC");
+  } else {
+    Serial.println("UNKNOWN");
   }
-  file.close();
-  esp_camera_fb_return(fb); 
-  
-  // Turn off the FLASH led
-  pinMode(4, OUTPUT);
-  digitalWrite(4, LOW);
 
-  delay(2000);
-  //Serial.println("Going to sleep now");
-  //delay(2000);
-  //esp_deep_sleep_start();
-  //Serial.println("This will never be printed");
+  sd_sign = true;
 
-  String filename = "/welcome.txt"; // Specify the filename
-  String data = "Welcome to CubeSat"; // Data to be saved
-  char mode = 'w'; // Specify the mode ('a' for append or 'w' for write)
+  setTime(0);
 
-  save_to_file(filename,data,mode);
-
+  Serial.println("Photos will begin every 5 seconds, please be ready.");
 }
 
-/* --------------------------- LOOP --------------------------- */
-
 void loop() {
+  if(camera_sign && sd_sign){
+    unsigned long now = millis();
   
+    if ((now - lastCaptureTime) >= 5000) {
+      // Take photo
+      char filename[32];
+      sprintf(filename, "/image%d.jpg", imageCount);
+      photo_save(filename);
+      Serial.printf("Saved picture: %s\r\n", filename);
+
+      Serial.println("Photos will begin in one minute, please be ready.");
+      imageCount++;
+      lastCaptureTime = now;
+    }
+
+    // Save to data file
+    char timeData[50];
+    sprintf(timeData, "Data was stored at time %02d:%02d:%02d", hour(), minute(), second());
+    appendFile("/data.txt", timeData);
+  }
 }
