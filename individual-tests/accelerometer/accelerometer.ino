75113c474be200bbec41d9f902de5fc758ddf858
Tony <santiago.montesb@gmail.com>
2024-05-22 12:43:07 -0500

    updated barometer and accelerometer

--- individual-tests/accelerometer/accelerometer.ino
+++ individual-tests/accelerometer/accelerometer.ino
@@ -1,112 +1,171 @@
-#include <Adafruit_MPU6050.h>
-#include <Adafruit_Sensor.h>
 #include <Wire.h>
+#include <MechaQMC5883.h>	
 
-Adafruit_MPU6050 mpu;
+MechaQMC5883 qmc;
 
-void setup(void) {
-  Serial.begin(115200);
-  while (!Serial)
-    delay(10);
+float RateRoll, RatePitch, RateYaw;
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
+int AccCalibrationNumber;
+float AccX, AccY, AccZ;
+float AccXCalibration, AccYCalibration, AccZCalibration;
+float AngleRoll, AnglePitch, AngleYaw;
+float elapsedTime, currentTime, previousTime;
+uint32_t LoopTimer;
+float KalmanAngleRoll=0, KalmanUncertaintyAngleRoll=4*4;
+float KalmanAnglePitch=0, KalmanUncertaintyAnglePitch=4*4;
+float KalmanAngleYaw=-60, KalmanUncertaintyAngleYaw=4*4;
+float Kalman1DOutput[]={0,0};
+int mx_bias=-287;
+int my_bias=239;
+int mz_bias=275;
 
-  Serial.println("Adafruit MPU6050 test!");
+float Yaw;
+bool calibrated = false;
 
-  if (!mpu.begin()) {
-    Serial.println("Failed to find MPU6050 chip");
-    //while (1) {
-    //  delay(10);
-    //}
-  }
-  Serial.println("MPU6050 Found!");
-
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  Serial.print("Accelerometer range set to: ");
-  switch (mpu.getAccelerometerRange()) {
-  case MPU6050_RANGE_2_G:
-    Serial.println("+-2G");
-    break;
-  case MPU6050_RANGE_4_G:
-    Serial.println("+-4G");
-    break;
-  case MPU6050_RANGE_8_G:
-    Serial.println("+-8G");
-    break;
-  case MPU6050_RANGE_16_G:
-    Serial.println("+-16G");
-    break;
-  }
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  Serial.print("Gyro range set to: ");
-  switch (mpu.getGyroRange()) {
-  case MPU6050_RANGE_250_DEG:
-    Serial.println("+- 250 deg/s");
-    break;
-  case MPU6050_RANGE_500_DEG:
-    Serial.println("+- 500 deg/s");
-    break;
-  case MPU6050_RANGE_1000_DEG:
-    Serial.println("+- 1000 deg/s");
-    break;
-  case MPU6050_RANGE_2000_DEG:
-    Serial.println("+- 2000 deg/s");
-    break;
-  }
-
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
-  Serial.print("Filter bandwidth set to: ");
-  switch (mpu.getFilterBandwidth()) {
-  case MPU6050_BAND_260_HZ:
-    Serial.println("260 Hz");
-    break;
-  case MPU6050_BAND_184_HZ:
-    Serial.println("184 Hz");
-    break;
-  case MPU6050_BAND_94_HZ:
-    Serial.println("94 Hz");
-    break;
-  case MPU6050_BAND_44_HZ:
-    Serial.println("44 Hz");
-    break;
-  case MPU6050_BAND_21_HZ:
-    Serial.println("21 Hz");
-    break;
-  case MPU6050_BAND_10_HZ:
-    Serial.println("10 Hz");
-    break;
-  case MPU6050_BAND_5_HZ:
-    Serial.println("5 Hz");
-    break;
-  }
-
-  Serial.println("");
-  delay(100);
+void kalman_1d(float KalmanState, float KalmanUncertainty, float KalmanInput, float KalmanMeasurement) {
+  KalmanState=KalmanState+0.004*KalmanInput;
+  KalmanUncertainty=KalmanUncertainty + 0.004 * 0.004 * 4 * 4;
+  float KalmanGain=KalmanUncertainty * 1/(1*KalmanUncertainty + 3 * 3);
+  KalmanState=KalmanState+KalmanGain * (KalmanMeasurement-KalmanState);
+  KalmanUncertainty=(1-KalmanGain) * KalmanUncertainty;
+  Kalman1DOutput[0]=KalmanState; 
+  Kalman1DOutput[1]=KalmanUncertainty;
 }
+void gyro_signals(void) {
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C);
+  Wire.write(0x10);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission(); 
+  Wire.requestFrom(0x68,6);
+  int16_t AccXLSB = Wire.read() << 8 | Wire.read();
+  int16_t AccYLSB = Wire.read() << 8 | Wire.read();
+  int16_t AccZLSB = Wire.read() << 8 | Wire.read();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B); 
+  Wire.write(0x8);
+  Wire.endTransmission();     
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68,6);
+  int16_t GyroX=Wire.read()<<8 | Wire.read();
+  int16_t GyroY=Wire.read()<<8 | Wire.read();
+  int16_t GyroZ=Wire.read()<<8 | Wire.read();
+  RateRoll=(float)GyroX/65.5;
+  RatePitch=(float)GyroY/65.5;
+  RateYaw=(float)GyroZ/65.5;
+  AccX=(float)AccXLSB/4096;//+0.01;
+  AccY=(float)AccYLSB/4096;//-0.01;
+  AccZ=(float)AccZLSB/4096;//-0.12;
+  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
+  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+}
+
+void gyro_calibration(void){
+  for (RateCalibrationNumber=0; RateCalibrationNumber<4000; RateCalibrationNumber ++) {
+    gyro_signals();
+    RateCalibrationRoll+=RateRoll;
+    RateCalibrationPitch+=RatePitch;
+    RateCalibrationYaw+=RateYaw;
+    AccXCalibration+=AccX;
+    AccYCalibration+=AccY;
+    AccZCalibration+=AccZ;
+    delay(1);
+  }
+  RateCalibrationRoll/=4000;
+  RateCalibrationPitch/=4000;
+  RateCalibrationYaw/=4000;
+  AccXCalibration/=4000;
+  AccYCalibration/=4000;
+  AccZCalibration/=4000;
+}
+
+
+void setup() {
+  pinMode(4,OUTPUT);
+  Serial.begin(115200);
+  pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+  LoopTimer=micros();
+}
+
 
 void loop() {
 
-  sensors_event_t a, g, temp;
-  mpu.getEvent(&a, &g, &temp);
+  uint16_t dataSerial = 0;
+  uint16_t dataValue = 0;
+  uint8_t dataCode = 0;
 
-  Serial.print("Acceleration X: ");
-  Serial.print(a.acceleration.x);
-  Serial.print(", Y: ");
-  Serial.print(a.acceleration.y);
-  Serial.print(", Z: ");
-  Serial.print(a.acceleration.z);
-  Serial.println(" m/s^2");
+  if (Serial.available() > 0){
+    dataSerial = Serial.parseInt();  // Returns a Zero after reading
+    if (Serial.read() == '\n'){} // kills 1 second wait
+    // Works for 5 digit values. Can be used with String(value).length().
+    if (dataSerial > 99){
+      dataCode = dataSerial / 1000;
+      dataValue = dataSerial % 1000;
+    } else {
+      dataCode = dataSerial;
+    }
+  }
 
-  Serial.print("Rotation X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
-  Serial.print(g.gyro.z);
-  Serial.println(" rad/s");
+  switch(dataCode){
+    case 11:
+      gyro_calibration();
+      calibrated = true;
+    default:
+      gyro_signals();
+      int mx,my,mz,acimut;
+      qmc.read(&mx,&my,&mz,&acimut);
+      mx -= mx_bias;
+      my -= my_bias;
+      mz -= mz_bias;
+      AngleYaw = atan2(my,mx)*(180/3.1415);
+      if (calibrated){
+        RateRoll-=RateCalibrationRoll;
+        RatePitch-=RateCalibrationPitch;
+        RateYaw-=RateCalibrationYaw;
+        AccX-=AccXCalibration;
+        AccY-=AccYCalibration;
+        AccZ = AccZ - AccZCalibration +1;
+      }
+      kalman_1d(KalmanAngleRoll, KalmanUncertaintyAngleRoll, RateRoll, AngleRoll);
+      KalmanAngleRoll=Kalman1DOutput[0]; 
+      KalmanUncertaintyAngleRoll=Kalman1DOutput[1];
+      kalman_1d(KalmanAnglePitch, KalmanUncertaintyAnglePitch, RatePitch, AnglePitch);
+      KalmanAnglePitch=Kalman1DOutput[0]; 
+      KalmanUncertaintyAnglePitch=Kalman1DOutput[1];
+      kalman_1d(KalmanAngleYaw, KalmanUncertaintyAngleYaw, RateYaw, AngleYaw);
+      KalmanAngleYaw=Kalman1DOutput[0]; 
+      KalmanUncertaintyAngleYaw=Kalman1DOutput[1];
+      //Serial.print("Roll [deg]: "); 
+      Serial.print(KalmanAngleRoll);Serial.print(",");
+      //Serial.print("Pitch [deg]: "); 
+      Serial.print(KalmanAnglePitch);Serial.print(",");
+      //Serial.print("Yaw [deg]: "); 
+      Serial.println(KalmanAngleYaw);//Serial.println(",");
 
-  Serial.print("Temperature: ");
-  Serial.print(temp.temperature);
-  Serial.println(" degC");
 
-  Serial.println("");
-  delay(500);
+      while (micros() - LoopTimer < 4000);
+      LoopTimer=micros();
+  }
+}
+
+float degTorad(float angle){
+  float rad_angle = angle * (3.1415/180);
+  return rad_angle;
 }
--- individual-tests/barometer/barometer.ino
+++ individual-tests/barometer/barometer.ino
@@ -1,34 +1,60 @@
 #include <Wire.h>
-#include <BMP280_DEV.h>
+#include <SPI.h>
+#include <Adafruit_BMP280.h>
 
-#define I2C_SDA 14
-#define I2C_SCL 15
-BMP280_DEV bmp(I2C_SDA, I2C_SCL);
+#define BMP_SCK  (13)
+#define BMP_MISO (12)
+#define BMP_MOSI (11)
+#define BMP_CS   (10)
 
-float temperature, pressure, altitude;
+Adafruit_BMP280 bmp; // I2C
+//Adafruit_BMP280 bmp(BMP_CS); // hardware SPI
+//Adafruit_BMP280 bmp(BMP_CS, BMP_MOSI, BMP_MISO,  BMP_SCK);
 
 void setup() {
-  Serial.begin(115200);
-
-  Wire.begin(I2C_SDA, I2C_SCL);
-
-  Serial.println("Testing BMP280...");
-  if (!bmp.begin(0x76)) {
-    Serial.print("Oops...something went wrong... please check your BMP280 sensor");
-    //while (1);
+  Serial.begin(9600);
+  while ( !Serial ) delay(100);   // wait for native usb
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  //status = bmp.begin(BMP280_ADDRESS_ALT, BMP280_CHIPID);
+  status = bmp.begin(0x76);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
   }
-  bmp.setTimeStandby(TIME_STANDBY_2000MS);
-  bmp.startNormalConversion();
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
+                  Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
+                  Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
+                  Adafruit_BMP280::FILTER_X16,      /* Filtering. */
+                  Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
 }
 
 void loop() {
-  // BMP280 readings need to be on loop to get accurate readings
-  bmp.getMeasurements(temperature, pressure, altitude);
+    Serial.print(F("Temperature = "));
+    Serial.print(bmp.readTemperature());
+    Serial.println(" *C");
 
-  Serial.print("Temperature: ");
-  Serial.println(temperature);
-  Serial.print("Pressure: ");
-  Serial.println(pressure);
-  Serial.print("Altitude: ");
-  Serial.println(altitude);
+    float pressure = bmp.readPressure();
+    float new_pressure = 0.968*pressure +2287.2;
+
+    Serial.print(F("Pressure = "));
+    Serial.print(new_pressure);
+    Serial.println(" Pa");
+    
+    float alt = bmp.readAltitude(1011.9);
+    float new_alt = 1.48*alt - 1071.9;
+
+    Serial.print(F("Approx altitude = "));
+    Serial.print(new_alt); /* Adjusted to local forecast! */
+    Serial.println(" m");
+
+    Serial.println();
+    delay(1000);
 }
