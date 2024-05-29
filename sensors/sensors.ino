a84ffb71927989e5af9da0f01c63e7b08ed53cc2
Tony Montes <69558777+t-montes@users.noreply.github.com>
2024-05-29 17:10:16 -0500

    Update sensors.ino

--- sensors/sensors.ino
+++ sensors/sensors.ino
@@ -22,6 +22,18 @@ const unsigned long connectionInterval = 1000;
 int i = 0;
 String data;
 
+float RateRoll, RatePitch, RateYaw;
+float RateCalibrationRoll, RateCalibrationPitch, RateCalibrationYaw;
+int RateCalibrationNumber;
+int AccCalibrationNumber;
+float AccX, AccY, AccZ;
+float AccXCalibration, AccYCalibration, AccZCalibration;
+float AngleRoll, AnglePitch, AngleYaw;
+
+int mx_bias=-287;
+int my_bias=239;
+int mz_bias=275;
+
 String accx = "0.0", accy = "0.0", accz = "0.0", roll = "0.0", yaw = "0.0", pitch = "0.0"; // accelerometer
 String rollg = "0.0", yawg = "0.0", pitchg = "0.0"; // magnetometer
 String pressure = "0.0", altitude = "0.0", temperature = "0.0"; // barometer
@@ -29,6 +41,7 @@ String altitudegps = "0.0", lat = "0.0", lon = "0.0"; // gps
 String camera = "0.0", vbat = "0.0", vpv = "0.0"; // own
 
 void setup() {
+  Serial.println("Init");
   Serial.begin(9600);
   serial_connection.begin(9600);
   Serial.println("GPS Start");
@@ -37,7 +50,7 @@ void setup() {
   unsigned status = bmp.begin(0x76);
   if (!status) {
     Serial.println(F("Could not find a valid BMP280 sensor, check wiring or try a different address!"));
-    while (1) delay(10);
+    //while (1) delay(10);
   }
   bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
                   Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
@@ -47,6 +60,75 @@ void setup() {
 
   Wire.begin();
   qmc.init();
+  Wire.setClock(400000);
+  Wire.begin();
+  delay(250);
+  Wire.beginTransmission(0x68); 
+  Wire.write(0x6B);
+  Wire.write(0x00);
+  Wire.endTransmission();
+}
+
+void accelerometerRead(){
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1A);
+  Wire.write(0x05);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1C);
+  Wire.write(0x10);
+  Wire.endTransmission();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);
+  Wire.endTransmission(); 
+  Wire.requestFrom(0x68,6);
+  int16_t AccXLSB = Wire.read() << 8 | Wire.read();
+  int16_t AccYLSB = Wire.read() << 8 | Wire.read();
+  int16_t AccZLSB = Wire.read() << 8 | Wire.read();
+  Wire.beginTransmission(0x68);
+  Wire.write(0x1B); 
+  Wire.write(0x8);
+  Wire.endTransmission();     
+  Wire.beginTransmission(0x68);
+  Wire.write(0x43);
+  Wire.endTransmission();
+  Wire.requestFrom(0x68,6);
+  int16_t GyroX=Wire.read()<<8 | Wire.read();
+  int16_t GyroY=Wire.read()<<8 | Wire.read();
+  int16_t GyroZ=Wire.read()<<8 | Wire.read();
+  RateRoll=(float)GyroX/65.5;
+  RatePitch=(float)GyroY/65.5;
+  RateYaw=(float)GyroZ/65.5;
+  AccX=(float)AccXLSB/4096;//+0.01;
+  AccY=(float)AccYLSB/4096;//-0.01;
+  AccZ=(float)AccZLSB/4096;//-0.12;
+  AngleRoll=atan(AccY/sqrt(AccX*AccX+AccZ*AccZ))*1/(3.142/180);
+  AnglePitch=-atan(AccX/sqrt(AccY*AccY+AccZ*AccZ))*1/(3.142/180);
+  roll = String(AngleRoll);
+  pitch = String(AnglePitch);
+
+  accx = String(AccX);
+  accy = String(AccY);
+  accz = String(AccZ);
+}
+
+void accelerometerCalibration(){
+  for (RateCalibrationNumber=0; RateCalibrationNumber<2000; RateCalibrationNumber ++) {
+    accelerometerRead();
+    RateCalibrationRoll+=RateRoll;
+    RateCalibrationPitch+=RatePitch;
+    RateCalibrationYaw+=RateYaw;
+    AccXCalibration+=AccX;
+    AccYCalibration+=AccY;
+    AccZCalibration+=AccZ;
+    delay(1);
+  }
+  RateCalibrationRoll/=4000;
+  RateCalibrationPitch/=4000;
+  RateCalibrationYaw/=4000;
+  AccXCalibration/=4000;
+  AccYCalibration/=4000;
+  AccZCalibration/=4000;
 }
 
 void magnetometerRead() {
@@ -55,6 +137,9 @@ void magnetometerRead() {
   rollg = String(x);
   yawg = String(y);
   pitchg = String(z);
+
+  AngleYaw = atan2(y,x)*(180/3.1415);
+  yaw = String(AngleYaw);
 }
 
 void barometerRead() {
@@ -88,9 +173,10 @@ void loop() {
 
       barometerRead();
       magnetometerRead();
+      accelerometerRead();
 
       data = String(now) + SEP +
-             // accelerometer
+             accx + SEP + accy + SEP + accz + SEP + roll + SEP + yaw + SEP + pitch + SEP + // accelerometer
              rollg + SEP + yawg + SEP + pitchg + SEP +
              temperature + SEP + pressure + SEP + altitude + SEP +
              altitudegps + SEP + lat + SEP + lon;
@@ -99,4 +185,7 @@ void loop() {
       Serial.println(data);
     }
   }
+
+  // datos listos
+  
 }
